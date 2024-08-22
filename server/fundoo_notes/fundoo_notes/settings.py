e45e7388655e2702b7249b98494f5341f6c6325a
Aniket2659 <aniket2659@gmail.com>
2024-08-22 12:45:04 +0530

    [Aniket] Add: added restframework app

--- server/fundoo_notes/fundoo_notes/settings.py
+++ server/fundoo_notes/fundoo_notes/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_auth'
+    'user_auth',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
